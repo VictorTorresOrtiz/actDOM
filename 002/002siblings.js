051b05b18d6541e9efa82a7313b1ddfe3f391850
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-12-02 09:48:29 +0100

    002

--- 002/002siblings.js
+++ 002/002siblings.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 1. verdadero. El elemento elem.lastChild siempre es el último y por eso no tiene nextSibling.
 2. Falso. elem.children[0] es el primer hijo entre elementos, pueden existir nodos que no son elementos antes que él. previousSibling puede ser un nodo texto.
