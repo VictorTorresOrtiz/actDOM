43cf485f33636a3006a9a9b76a7566a40bf5c9b6
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-12-05 09:37:45 +0100

    HACER

--- 008/008whereIsDocument..js
+++ 008/008whereIsDocument..js
@@ -1,2 +1,19 @@
 "use strict";
 
+/*
+Dónde está el "document" en la jerarquía?
+
+
+¿A qué clase pertenece el document?
+Podemos ver la clase de "document" con alert(document) == [object HTMLDocument] (Instancia de HTMLDocument)
+
+
+
+¿Cuál es su lugar en la jerarquía DOM?
+HACER
+
+
+¿Hereda de Node o Element, o tal vez HTMLElement?
+HACER
+
+*/
