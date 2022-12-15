da2d3ab1bd506de1fe74306ffaf4d4a0b91926cf
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-12-15 16:54:33 +0100

    HECHO

---
+++ 017/017htmlToList.js
@@ -0,0 +1,6 @@
+"use strict"
+
+one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');
+
+//insertAdjacentHTML --> Analiza cadena txt e inserta en el DOM los nodos resultantes
+// afterend --> Posición del elemento justo antes del primer hijo
