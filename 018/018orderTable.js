81f3192d1a117f4b9d73e1c3ba0a247a5b01de5c
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-12-15 17:03:41 +0100

    HECHO

--- 018/018orderTable.js
+++ 018/018orderTable.js
@@ -1,2 +1,7 @@
 "use strict"
 
+let sortedRows = Array.from(tabla.tBodies[0].rows) // Obtengo todas las etiquetas <tr></tr> de tbody
+.sort((rowA, rowB) => rowA.cells[0].innerHTML.localeCompare(rowB.cells[0].innerHTML)); // Las ordeno teniendo en cuenta el campo de nombre
+
+tabla.tBodies[0].append(...sortedRows); //Utilizando .append introduzco todos los nodos por su orden correcto
+
