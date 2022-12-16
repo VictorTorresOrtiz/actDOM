79a669d1882e4df90f1d5a918e0e559882660f01
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-12-16 08:50:05 +0100

    TERMINADO

--- 020/020hideTextButton.js
+++ 020/020hideTextButton.js
@@ -1,5 +1,6 @@
 "use strict"
 
- document.getElementById('esconder').onclick = function() {
+ document.getElementById('esconder').onclick = function() { //Llamo al ID y con un onclick activo la función
+  
     document.getElementById('text').hidden = true; //hidden para esconder 
       }
