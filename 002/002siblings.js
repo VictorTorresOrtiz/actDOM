e44aed932f1275d6756c729572185b2b5129a550
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-12-21 20:45:27 +0100

    Corrección

--- 002/002siblings.js
+++ 002/002siblings.js
@@ -1,7 +1,9 @@
 "use strict";
-/*
-1. verdadero. El elemento elem.lastChild siempre es el último y por eso no tiene nextSibling.
-2. Falso. elem.children[0] es el primer hijo , pueden existir nodos que no son elementos antes que él. previousSibling puede ser un nodo texto.
 
-Si no hay hijos, elem.lastChild == null.
-*/
+//1 Es nulo por que es IMPOSIBLE coger el ultimo hermado si no hay "NADA"
+let elem = document.getElementById("item");
+alert(elem.lastChild.nextSibling);
+
+
+// 2 Ami me sale correcto pero buscando por internet he encontrado que es un error de JS
+alert(elem.children[0].previousSibling);
